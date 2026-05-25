9096b2f3e49f4ffa4b7c661ef08f5271f23a2441
Sagar Taunk <sagartaunk@proton.me>
2026-05-25 17:43:52 +0530

    main.rs : Add a dummy struct for testing
    Add a dummy struct `PendingOnce`, as the name implies, the future
    returns `pending` on the first poll and `Ready` on the second.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,37 @@
+use std::{
+    future::Pending,
+    pin::Pin,
+    task::{Context, Poll},
+};
+
 use crate::{executor::Executor, types::Task};
 
 mod executor;
 mod types;
 mod waker;
 fn main() {
-    let future = async {};
-    let task = Task::new(future);
+    let task = Task::new(PendingOnce::new());
     let mut exec = Executor::new();
     exec.add(task);
     exec.run();
+}
+
+struct PendingOnce {
+    polled: bool,
+}
+impl PendingOnce {
+    fn new() -> Self {
+        Self { polled: false }
+    }
+}
+impl Future for PendingOnce {
+    type Output = ();
+    fn poll(mut self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<Self::Output> {
+        if self.polled {
+            Poll::Ready(())
+        } else {
+            self.polled = true;
+            Poll::Pending
+        }
+    }
 }
