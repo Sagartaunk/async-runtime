98b18086b3c78d7eb63985e91b76f2298046cf08
Sagar Taunk <sagartaunk@proton.me>
2026-05-25 15:20:54 +0530

    types.rs: Implement `new` method on `Task` struct
    Implement `new` method on task struct which takes in a future,
    pins it and returns a `Task` struct. Moreover, added a simple test
    case to `main.rs`.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
+use crate::{executor::Executor, types::Task};
+
 mod executor;
 mod types;
 mod waker;
 fn main() {
-    println!("Hello, world!");
+    let future = async {};
+    let task = Task::new(future);
+    let mut exec = Executor::new();
+    exec.add(task);
+    exec.run();
 }
--- src/types.rs
+++ src/types.rs
@@ -1,7 +1,14 @@
+use std::future::Future;
 use std::pin::Pin;
-
 /// Storage for tasks.
 
 pub struct Task {
     pub task: Pin<Box<dyn Future<Output = ()>>>,
+}
+impl Task {
+    pub fn new(fut: impl Future<Output = ()> + 'static) -> Self {
+        Self {
+            task: Box::pin(fut),
+        }
+    }
 }
