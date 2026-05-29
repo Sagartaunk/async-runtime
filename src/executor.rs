8d23d850c1c71ca3cd27a84015fd438264800348
Sagar Taunk <sagartaunk@proton.me>
2026-05-29 16:20:55 +0530

    executor.rs: Implement `Reactor`
    Add a field `reactor` to the `Executor` struct which holds an
    `Arc<Mutex<Reactor>>`. Edit `new()` method on `Executor` to take
    in a `Arc<Mutex<Reactor>>`. Moreover, previously the `run` method
    exited when `self.queue` was empty, now it calls `wait` method of
    the `Reactor` struct and waits for proceed events to fire.

--- src/executor.rs
+++ src/executor.rs
@@ -1,19 +1,20 @@
+use crate::reactor::Reactor;
 use crate::types::Task;
 use crate::waker::task_waker;
 use std::collections::VecDeque;
-use std::future::Future;
-use std::pin::Pin;
 use std::sync::{Arc, Mutex};
-use std::task::{Context, Poll, RawWaker, RawWakerVTable, Waker};
+use std::task::{Context, Poll};
 
 /// Task Executor.
 pub struct Executor {
     queue: Arc<Mutex<VecDeque<Arc<Mutex<Task>>>>>,
+    reactor: Arc<Mutex<Reactor>>,
 }
 impl Executor {
-    pub fn new() -> Self {
+    pub fn new(reactor: Arc<Mutex<Reactor>>) -> Self {
         Self {
             queue: Arc::new(Mutex::new(VecDeque::new())),
+            reactor: reactor,
         }
     }
     pub fn add(&mut self, task: Arc<Mutex<Task>>) {
@@ -26,19 +27,27 @@ impl Executor {
             .push_back(Arc::new(Mutex::new(task)));
     }
     pub fn run(&mut self) {
-        while let Some(task) = {
-            let mut t = self.queue.lock().unwrap();
-            t.pop_front()
-        } {
-            let waker = task_waker(Arc::clone(&task), Arc::clone(&self.queue));
-            let mut context = Context::from_waker(&waker);
-            let result = {
-                let mut t = task.lock().unwrap();
-                t.task.as_mut().poll(&mut context)
-            };
-            match result {
-                Poll::Ready(()) => {}
-                Poll::Pending => self.add(task),
+        loop {
+            let task = {
+                let mut t = self.queue.lock().unwrap();
+                t.pop_front()
+            }; // lock dropped here
+            match task {
+                Some(task) => {
+                    let waker = task_waker(Arc::clone(&task), Arc::clone(&self.queue));
+                    let mut context = Context::from_waker(&waker);
+                    let result = {
+                        let mut t = task.lock().unwrap();
+                        t.task.as_mut().poll(&mut context)
+                    };
+                    match result {
+                        Poll::Ready(()) => {}
+                        Poll::Pending => self.add(task),
+                    }
+                }
+                None => {
+                    self.reactor.lock().unwrap().wait();
+                }
             }
         }
     }
